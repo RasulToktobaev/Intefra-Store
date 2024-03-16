9acb8575c0f0a568b9869951a7ccc3e12b7f080b
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-03-17 01:28:48 +0600

    correction

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,42 +1,40 @@
 import brandOne from '../../assets/images/brandOne.png'
-
+import brandTwo from '../../assets/images/BrandTwo.png'
+import brandThree from '../../assets/images/brandThree.png'
+import brandFour from '../../assets/images/BrandFour.png'
+import brandFive from '../../assets/images/brandFive.png'
+import brandSix from '../../assets/images/brandSix.png'
 
 export function Brand() {
     return(
         <div>
             <h2 className="brand-title">ТОП-БРЕНДЫ</h2>
-            <div className="flex ml-20">
-                <div>
+            <div className="container flex">
+                <div className="border-solid px-6 border-r border-gray">
                     <img src={brandOne} alt=""/>
-                    <span className="brand-line"></span>
                 </div>
-                <div>
-                    <div className="brand-image ml-20"></div>
-                    <span className="brand-line"></span>
+                <div className="border-solid px-6 border-r border-gray">
+                    <img src={brandTwo} alt=""/>
                 </div>
-                <div>
-                    <div className="brand-image ml-20"></div>
-                    <span className="brand-line"></span>
+                <div className="border-solid px-6 border-r border-gray">
+                    <img src={brandThree} alt=""/>
                 </div>
-                <div>
-                    <div className="brand-image ml-20"></div>
-                    <span className="brand-line"></span>
+                <div className="border-solid px-6 border-r border-gray">
+                    <img src={brandFour} alt=""/>
                 </div>
-                <div>
-                    <div className="brand-image ml-20"></div>
-                    <span className="brand-line"></span>
+                <div className="border-solid px-6 border-r border-gray">
+                    <img src={brandFive} alt=""/>
                 </div>
-                <div>
-                    <div className="brand-image ml-20"></div>
-                    <span className="brand-line"></span>
+                <div className="">
+                    <img src={brandSix} alt=""/>
                 </div>
+
             </div>
 
             <div className="flex justify-center mt-64 relative">
                 <h4 className="brand-text">ВСЕ БРЕНДЫ</h4>
                 <span className="brand__line"></span>
             </div>
-
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import {Gender} from "../../components/gender";
 import {Brand} from "../../components/brand";
+import {Collection} from "../../components/collection";
+import {Product} from "../../components/product";
 
 export function Home() {
     return <>
         <Gender/>
         <Brand/>
-
+        <Collection/>
+        <Product/>
     </>
 }
