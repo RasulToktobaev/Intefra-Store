add501dac86851543ecffc287282ee5785916d43
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-03-17 19:05:02 +0600

    add style

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,3 +1,17 @@
-export function Product () {
+import {ReactComponent as ProductLogo} from "../../assets/vectors/product.svg";
+import {ReactComponent as Phone} from "../../assets/vectors/product-phone.svg";
 
+export function Product() {
+    return (
+        <div>
+            <div className="product__content">
+                <ProductLogo/>
+                <p className="product-text">Изделия премиального качества из самых лучших материалов, ориентируясь на <br/> традиции итальянских производителей.</p>
+                <p className="text-white mt-10 border-solid border-b border-gray px-6">СМОТРЕТЬ КОЛЛЕКЦИЮ</p>
+            </div>
+            <div className="Phone container">
+                <Phone/>
+            </div>
+        </div>
+    )
 }
