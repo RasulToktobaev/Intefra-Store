7a0e00a8163dd386b71576740174f40f7d0685d2
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-03-20 21:19:32 +0600

    swiper fix, adding custom arrows

--- src/components/prices/Prices.jsx
+++ src/components/prices/Prices.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useRef, useCallback} from 'react';
 import item1 from "../../assets/images/newsItem1.png"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper/modules';
 import item2 from "../../assets/images/newsItem2.png"
 import item3 from "../../assets/images/newsItem3.png"
+import {FaChevronRight, FaChevronLeft} from "react-icons/fa6";
 
-export function Prices () {
+
+export function Prices() {
+
+    const sliderRef = useRef(null)
+
+    const handlePrev = useCallback(() => {
+        if (!sliderRef.current) return;
+        sliderRef.current.swiper.slidePrev();
+    }, []);
+
+    const handleNext = useCallback(() => {
+        if (!sliderRef.current) return;
+        sliderRef.current.swiper.slideNext();
+    }, []);
+
     return (
-            <section className="items">
-                <div className="container">
-                    <h2 className="font-semibold text-center text-4xl my-10 " style={{ color: 'red', fontSize:"55px", marginBottom:'10%', marginTop:'90px', fontWeight:"300" }}>СКИДКИ</h2>
+        <section className="items">
+            <div className="container">
+                <h2 className="font-semibold text-center text-4xl my-10 " style={{
+                    color: 'red',
+                    fontSize: "55px",
+                    marginBottom: '10%',
+                    marginTop: '90px',
+                    fontWeight: "300"
+                }}>СКИДКИ</h2>
+
+                <div className="relative">
                     <Swiper
                         modules={[Navigation]}
-                        navigation={true}
+                        ref={sliderRef}
                         spaceBetween={35}
                         slidesPerView={4}
                         loop={true}
@@ -20,60 +43,101 @@ export function Prices () {
                         onSwiper={(swiper) => console.log(swiper)}
                     >
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item1} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">Saint Laurent</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item2} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item3} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item1} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">Saint Laurent</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item2} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div >
+                            <div>
                                 <img src={item3} alt="Slide 1" className="swiper-image relative"/>
-                                <div className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl" style={{background:"red"}}>-70%</div>
+                                <div
+                                    className="absolute top-0 right-0 text-white font-semibold rounded-full px-4 py-4 text-2xl"
+                                    style={{background: "red"}}>-70%
+                                </div>
                                 <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span style={{ color: 'red', fontWeight:'700' }}> 96 000 ₽</span></p>
+                                <p className="text-center mt-2"><span className="line-through">123 000 ₽</span> <span
+                                    style={{color: 'red', fontWeight: '700'}}> 96 000 ₽</span></p>
                             </div>
                         </SwiperSlide>
-
-
                     </Swiper>
-
+                    <div className="absolute top-1/2 z-10 w-full">
+                        <button
+                            onClick={handlePrev}
+                            className="border-solid border border-gray stroke-gray w-[60px] h-[60px] grid place-items-center bg-transparent absolute  left-[-70px]">
+                            <FaChevronLeft
+                                size="34"
+                            />
+                        </button>
+                        <button
+                            onClick={handleNext}
+                            className=" border-solid border border-gray stroke-gray w-[60px] h-[60px] grid place-items-center bg-transparent absolute  right-[-70px]">
+                            <FaChevronRight
+                                size="34"
+                            />
+                        </button>
+                    </div>
                 </div>
-                <p style={{marginTop:'10%'}} className="text-base font-semibold uppercase text-center border-solid border-b border-gray w-fit mx-auto">СМОТРЕТЬ
-                    ВСЕ</p>
-            </section>
+
+
+            </div>
+            <p style={{marginTop: '10%'}}
+               className="text-base font-semibold uppercase text-center border-solid border-b border-gray w-fit mx-auto">СМОТРЕТЬ
+                ВСЕ</p>
+        </section>
     )
 }
--- src/components/swiper/Swiper.jsx
+++ src/components/swiper/Swiper.jsx
@@ -1,47 +1,80 @@
-import React from 'react';
+import React, {useCallback, useRef} from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/pagination';
 import item1 from "../../assets/images/newsItem1.png"
 import item2 from "../../assets/images/newsItem2.png"
 import item3 from "../../assets/images/newsItem3.png"
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa6";
 
 export function Items() {
+
+    const sliderRef = useRef(null)
+
+    const handlePrev = useCallback(() => {
+        if (!sliderRef.current) return;
+        sliderRef.current.swiper.slidePrev();
+    }, []);
+
+    const handleNext = useCallback(() => {
+        if (!sliderRef.current) return;
+        sliderRef.current.swiper.slideNext();
+    }, []);
+
     return (
         <section className="items mt-28">
             <div className="container">
                 <h2 className=" items-title font-semibold text-center text-3xl mb-16">НОВИНКИ</h2>
                 <p className=" items-text text-sm text-gray-500 mb-4 text-center">643 ОБНОВЛЕНИЙ</p>
-                <Swiper modules={[Navigation]}
-                        navigation={true}
-                        spaceBetween={35}
-                        slidesPerView={4}
-                        loop={true}
-                        onSlideChange={() => console.log('slide change')}
-                        onSwiper={(swiper) => console.log(swiper)}
-                >
-                    <SwiperSlide>
-                        <img src={item1} alt="Slide 1" className="swiper-image"/>
-                        <p className="swiper-text">Saint Laurent</p>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={item2} alt="Slide 2" className="swiper-image"/>
-                        <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={item3} alt="Slide 3" className="swiper-image"/>
-                        <p className="swiper-text">BRUNELLO CUCINELLI</p>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={item3} alt="Slide 4" className="swiper-image"/>
-                        <p className="swiper-text">Saint Laurent</p>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={item1} alt="Slide 5" className="swiper-image"/>
-                        <p className="swiper-text">Saint Laurent</p>
-                    </SwiperSlide>
 
-                </Swiper>
+                <div className="relative">
+                    <Swiper modules={[Navigation]}
+                            ref={sliderRef}
+                            spaceBetween={35}
+                            slidesPerView={4}
+                            loop={true}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}
+                    >
+                        <SwiperSlide>
+                            <img src={item1} alt="Slide 1" className="swiper-image "/>
+                            <p className="swiper-text">Saint Laurent</p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={item2} alt="Slide 2" className="swiper-image "/>
+                            <p className="swiper-text">BRUNELLO CUCINELLI</p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={item3} alt="Slide 3" className="swiper-image "/>
+                            <p className="swiper-text">BRUNELLO CUCINELLI</p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={item3} alt="Slide 4" className="swiper-image"/>
+                            <p className="swiper-text">Saint Laurent</p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={item1} alt="Slide 5" className="swiper-image"/>
+                            <p className="swiper-text">Saint Laurent</p>
+                        </SwiperSlide>
+                    </Swiper>
+                    <div className="absolute  z-10 w-full">
+                        <button
+                            onClick={handlePrev}
+                            className="border-solid border border-gray stroke-gray w-[60px] h-[60px] grid place-items-center bg-transparent absolute  left-[-70px] bottom-[150px]">
+                            <FaChevronLeft
+                                size="34"
+                            />
+                        </button>
+                        <button
+                            onClick={handleNext}
+                            className=" border-solid border border-gray stroke-gray w-[60px] h-[60px] grid place-items-center bg-transparent absolute  right-[-70px] bottom-[150px] ">
+                            <FaChevronRight
+                                size="34"
+                            />
+                        </button>
+                    </div>
+                </div>
+
 
             </div>
             <p className="text-base font-semibold uppercase text-center border-solid border-b border-gray w-fit mx-auto mt-20">СМОТРЕТЬ
