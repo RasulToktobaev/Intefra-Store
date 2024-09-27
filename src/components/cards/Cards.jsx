05dc862867fa70267574c1cda328e36a3edf8838
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-09-27 18:52:05 +0600

    replace card in jsx

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,3 +1,50 @@
+// import {useDispatch, useSelector} from "react-redux";
+// import {useEffect} from "react";
+// import {getCollection} from "../../store/reducers/clothes/clothes";
+// import {useNavigate} from "react-router-dom";
+// import {animateScroll as scroll} from "react-scroll";
+
+// export function Cards() {
+//     const {data} = useSelector((state) => state.clothes)
+
+//     const dispatch = useDispatch()
+//     const navigate = useNavigate()
+
+//     const scrollToTop = () => {
+//         scroll.scrollToTop();
+//     };
+
+//     useEffect(() => {
+//         dispatch(getCollection())
+//         scrollToTop()
+//     }, []);
+
+//     return (
+//         <div>
+//             <div className=" flex flex-wrap gap-8 ml-28 mt-16 mb-10">
+//                 {
+//                     data.map((item) => (
+//                         <div>
+//                             <img onClick={() => navigate(`/product/${item.id}`)} className="card-image"
+//                                  src={item.image.black} alt=""/>
+//                             <p className="collection-text">{item.title}</p>
+//                             <p style={{
+//                                 textAlign: "center",
+//                                 marginTop: '15px',
+//                                 fontSize: '18px',
+//                                 color: 'blue'
+//                             }}>Цена:{item.price} $</p>
+//                             <p style={{color: 'red', textAlign: 'center'}}>Цена со скидкой {item.priceSale} $</p>
+//                         </div>
+//                     ))
+//                 }
+//             </div>
+
+//         </div>
+
+//     )
+// }
+
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getCollection} from "../../store/reducers/clothes/clothes";
@@ -5,42 +52,64 @@ import {useNavigate} from "react-router-dom";
 import {animateScroll as scroll} from "react-scroll";
 
 export function Cards() {
-    const {data} = useSelector((state) => state.clothes)
+    const {data} = useSelector((state) => state.clothes);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const scrollToTop = () => {
         scroll.scrollToTop();
     };
 
     useEffect(() => {
-        dispatch(getCollection())
-        scrollToTop()
-    }, []);
+        dispatch(getCollection());
+        scrollToTop();
+    }, [dispatch]);
+
+    const handleImageClick = (id) => {
+        if (id) {
+            navigate(`/product/${id}`);
+        } else {
+            console.error('Product ID is not defined');
+        }
+    };
 
     return (
         <div>
-            <div className=" flex flex-wrap gap-8 ml-28 mt-16 mb-10">
-                {
+            <div className="flex flex-wrap gap-8 ml-28 mt-16 mb-10">
+                {data && data.length > 0 ? (
                     data.map((item) => (
-                        <div>
-                            <img onClick={() => navigate(`/product/${item.id}`)} className="card-image"
-                                 src={item.image.black} alt=""/>
+                        <div key={item.id}>
+                            {item.image && item.image.black ? (
+                                <img
+                                    onClick={() => handleImageClick(item.id)}
+                                    className="card-image"
+                                    src={item.image.black}
+                                    alt={item.title || 'Product Image'}
+                                />
+                            ) : (
+                                <p>Image not available</p>
+                            )}
                             <p className="collection-text">{item.title}</p>
-                            <p style={{
-                                textAlign: "center",
-                                marginTop: '15px',
-                                fontSize: '18px',
-                                color: 'blue'
-                            }}>Цена:{item.price} $</p>
-                            <p style={{color: 'red', textAlign: 'center'}}>Цена со скидкой {item.priceSale} $</p>
+                            <p
+                                style={{
+                                    textAlign: "center",
+                                    marginTop: "15px",
+                                    fontSize: "18px",
+                                    color: "blue",
+                                }}
+                            >
+                                Цена: {item.price} $
+                            </p>
+                            <p style={{color: "red", textAlign: "center"}}>
+                                Цена со скидкой {item.priceSale} $
+                            </p>
                         </div>
                     ))
-                }
+                ) : (
+                    <p>No items available</p>
+                )}
             </div>
-
         </div>
-
-    )
+    );
 }
--- src/components/collection/Collection.jsx
+++ src/components/collection/Collection.jsx
@@ -1,34 +1,44 @@
-import collectionOne from "../../assets/images/collection-one.png"
-import collectionTwo from "../../assets/images/collection-two.png"
-import collectionThree from "../../assets/images/collection-three.png"
-import {Link} from "react-router-dom";
-import {RoutesUrls} from "../../constans/routesUrls";
+import { Link } from 'react-router-dom'
+import collectionOne from '../../assets/images/collection-one.png'
+import collectionThree from '../../assets/images/collection-three.png'
+import collectionTwo from '../../assets/images/collection-two.png'
+import { RoutesUrls } from '../../constans/routesUrls'
 export function Collection() {
-    return (
-        <section className="collection">
-            <div className="container">
-                <div  className="collection-title">
-                    <h2>КОЛЛЕКЦИЯ</h2>
-                </div>
-                <div className="flex gap-12 ">
-                    <div>
-                        <img className="collection-img"  src={collectionOne} alt=""/>
-                        <p className="collection-text">Спортивный костюм Artigiani, футболка <br/> Artigiani, кеды Santoni</p>
-                    </div>
-                    <div>
-                        <img className="collection-img" src={collectionTwo} alt=""/>
-                        <p className="collection-text">Пуховый жилет Kiton, джинсы Jacob Cohen <br/>, футболка Fedeli, кроссовки  Valentino, <br/> бейсболка Kiton</p>
-                    </div>
-                    <div>
-                        <img className="collection-img" src={collectionThree} alt=""/>
-                        <p className="collection-text">Парка Artigiani, джемпер Artigiani <br/>, джинсы Jacob Cohen, челси Santoni</p>
-                    </div>
-                </div>
-                <div className= "collection-look border-solid px-6 border-b   w-56   border-gray mt-36 ">
-                    <Link  className="collection-more " to={RoutesUrls.collection.path}>СМОТРЕТЬ БОЛЬШЕ</Link>
-                </div>
-            </div>
-        </section>
-
-    )
+	return (
+		<section className="collection">
+			<div className="container">
+				<div className="collection-title">
+					<h2>КОЛЛЕКЦИЯ</h2>
+				</div>
+				<div className="flex gap-12 ">
+					<div>
+						<img className="collection-img" src={collectionOne} alt="" />
+						<p className="collection-text">
+							Спортивный костюм Artigiani, футболка <br /> Artigiani, кеды
+							Santoni
+						</p>
+					</div>
+					<div>
+						<img className="collection-img" src={collectionTwo} alt="" />
+						<p className="collection-text">
+							Пуховый жилет Kiton, джинсы Jacob Cohen <br />, футболка Fedeli,
+							кроссовки Valentino, <br /> бейсболка Kiton
+						</p>
+					</div>
+					<div>
+						<img className="collection-img" src={collectionThree} alt="" />
+						<p className="collection-text">
+							Парка Artigiani, джемпер Artigiani <br />
+							,джинсы Jacob Cohen, челси Santoni
+						</p>
+					</div>
+				</div>
+				<div className="collection-look border-solid px-6 border-b   w-56   border-gray mt-36 ">
+					<Link className="collection-more " to={RoutesUrls.collection.path}>
+						СМОТРЕТЬ БОЛЬШЕ
+					</Link>
+				</div>
+			</div>
+		</section>
+	)
 }
