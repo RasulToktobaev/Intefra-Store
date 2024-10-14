93120f5281de84a8438a81896815f05ca56462ea
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-10-14 22:11:27 +0600

    delete components

--- src/pages/collections/Collections.jsx
+++ src/pages/collections/Collections.jsx
@@ -1,13 +1,12 @@
-import {Work} from "../../components/work";
-import {Filtered} from "../../components/collections";
-import {Cards} from "../../components/cards";
+import { Filtered } from '../../components/collections'
+import { Work } from '../../components/work'
 
 export function Collections() {
-    return (
-        <>
-            <Filtered/>
-            <Cards/>
-            <Work/>
-        </>
-    )
+	return (
+		<>
+			<Filtered />
+			<Card />
+			<Work />
+		</>
+	)
 }
